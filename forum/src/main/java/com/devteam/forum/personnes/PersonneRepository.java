83348060e98171a89ca097c7d320313ebd2d990f
Issa MBOUP <issa.mboup314@gmail.com>
2022-05-30 00:07:39 +0300

    feat : filtre par mot de passe et pseudo

--- forum/src/main/java/com/devteam/forum/personnes/PersonneRepository.java
+++ forum/src/main/java/com/devteam/forum/personnes/PersonneRepository.java
@@ -3,5 +3,10 @@ package com.devteam.forum.personnes;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonneRepository extends CrudRepository<Personne, Long> {
+    Personne findByPseudoAndMotDePasse(String pseudo, String motDePasse);
+    Personne findByPseudo(String pseudo);
+
+
+
 
 }
