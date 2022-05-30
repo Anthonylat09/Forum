b6f21e5dc7a1402004678aedeb5c9ec55f4234b4
anthonylat09 <antoinendiaye09@gmail.com>
2022-05-30 23:48:54 +0200

    build: ajout de la dependance react-router-dom pour la navigation

--- forumfrontend/src/App.js
+++ forumfrontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import Inscription from './pages/Inscription'
 import Accueil from './pages/Accueil';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
 function App() {
+  const [token,setToken] = React.useState(null)
   return (
-  <Accueil/>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element= {<Inscription/>}/>
+        <Route path = "/accueil" element= {<Accueil/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
